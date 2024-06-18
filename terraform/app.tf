b9ea21def0149ef4d4ae631077fa1635bbece6e9
Prince Chime <chimeprince92@gmail.com>
2024-06-18 10:55:41 +0100

    modified configuration files by adding variables

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,38 +1,39 @@
-# resource "azurerm_resource_group" "example" {
-#   name     = "example-resources"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
-# resource "azurerm_virtual_network" "example" {
-#   name                = "virtnetname"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-# resource "azurerm_subnet" "example" {
-#   name                 = "subnetname"
-#   resource_group_name  = azurerm_resource_group.example.name
-#   virtual_network_name = azurerm_virtual_network.example.name
-#   address_prefixes     = ["10.0.2.0/24"]
-#   service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
-# }
+resource "azurerm_subnet" "snet" {
+  name                 = var.subnetname
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
 
-# resource "azurerm_storage_account" "example" {
-#   name                = "storageaccountqxr"
-#   resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_storage_account" "example" {
+  name                = var.storage_account_name
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   location                 = azurerm_resource_group.example.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-#   network_rules {
-#     default_action             = "Deny"
-#     ip_rules                   = ["100.0.0.1"]
-#     virtual_network_subnet_ids = [azurerm_subnet.example.id]
-#   }
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.snet.id]
+  }
 
-#   tags = {
-#     environment = "staging"
-#   }
-# }
+  tags = {
+    environment = "staging"
+  }
+}
+
---
+++ terraform/variable.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  type = string
+  description = "the name of the azure resource group"
+}
+
+variable "location" {
+  type = string
+  default = "west-europe"
+  description = "the name of the resource group location"
+}
+
+variable "subnetname" {
+  description = "name of the subnet"
+}
+
+variable "virtual_network_name" {
+  description = "name of vitual network"
+  type = string
+}
+
+variable "storage_account_name" {
+  description = "the name of the storage account"
+}
